3106c6994acaddc95d7013452fceeb99f143ed32
Jean-Eudes Marvie <jmarvie@nvidia.com>
2025-02-10 18:32:19 +0100

    indentation

--- src/utilities.h
+++ src/utilities.h
@@ -24,7 +24,7 @@
 #include <execution>
 #include <ranges>
 
-// Example using the parallel loop macro 
+// Example using the parallel loop macro
 // constexpr uint32_t N = 100;
 // START_PAR_LOOP( N, i)
 //   std::cout << "Processing index " << i << "\n";
@@ -33,17 +33,17 @@
 #if !defined(_WIN32)
 
 // Macro to start a parallel loop with a thread ID
-#define START_PAR_LOOP(SIZE, INDEX)                                                           \
-  {                                                                                                                    \
-    assert(THREAD_COUNT > 0);                                                                                          \
-    std::vector<std::thread>  _par_threads;                                                                            \
-    uint32_t                  _par_start = 0, _par_end = 0;                                                            \
-    uint32_t                 _par_num_threads = std::min((uint32_t)std::thread::hardware_concurrency(), (uint32_t)SIZE);      \
-    uint32_t                  _par_chunk_size  = (SIZE) / _par_num_threads;                                            \
-    uint32_t                  _par_remainder   = (SIZE) % _par_num_threads;                                            \
-    for(uint32_t _par_tid = 0; _par_tid < _par_num_threads; ++_par_tid)                                                \
-    {                                                                                                                  \
-      _par_end = _par_start + _par_chunk_size + (_par_tid < _par_remainder ? 1 : 0);                                   \
+#define START_PAR_LOOP(SIZE, INDEX)                                                                                      \
+  {                                                                                                                      \
+    assert(THREAD_COUNT > 0);                                                                                            \
+    std::vector<std::thread> _par_threads;                                                                               \
+    uint32_t                 _par_start = 0, _par_end = 0;                                                               \
+    uint32_t                 _par_num_threads = std::min((uint32_t)std::thread::hardware_concurrency(), (uint32_t)SIZE); \
+    uint32_t                 _par_chunk_size  = (SIZE) / _par_num_threads;                                               \
+    uint32_t                 _par_remainder   = (SIZE) % _par_num_threads;                                               \
+    for(uint32_t _par_tid = 0; _par_tid < _par_num_threads; ++_par_tid)                                                  \
+    {                                                                                                                    \
+      _par_end = _par_start + _par_chunk_size + (_par_tid < _par_remainder ? 1 : 0);                                     \
         _par_threads.emplace_back([&, _start=_par_start, _end=_par_end]() {                       \
             for (uint32_t INDEX = _start; INDEX < _end; ++INDEX) {
 
@@ -60,20 +60,20 @@
       _par_thread.join();                                                                                              \
     }                                                                                                                  \
   }                                                                                                                    \
-  }                                                                                                                    \
+  }
 
 #else
 
-#define START_PAR_LOOP(SIZE, INDEX) \
-{\
-    auto __range_ = std::views::iota(0, (int)SIZE); \
-    auto __begin_ = std::ranges::begin(__range_);\
-    auto __end_   = std::ranges::end(__range_);\
+#define START_PAR_LOOP(SIZE, INDEX)                                                                                    \
+  {                                                                                                                    \
+    auto __range_ = std::views::iota(0, (int)SIZE);                                                                    \
+    auto __begin_ = std::ranges::begin(__range_);                                                                      \
+    auto __end_   = std::ranges::end(__range_);                                                                        \
     std::for_each(std::execution::par, __begin_, __end_, [&](int INDEX) {
 
-#define END_PAR_LOOP() \
-    });\
-}
+#define END_PAR_LOOP()                                                                                                 \
+  });                                                                                                                \
+  }
 
 #endif
 
