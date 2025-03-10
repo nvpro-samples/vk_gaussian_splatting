4a7584a614165a89db8c827b9efe5cc50e168bf2
Jean-Eudes Marvie <jmarvie@nvidia.com>
2025-03-10 10:52:21 +0100

    add missing licence

--- shaders/dist.comp.glsl
+++ shaders/dist.comp.glsl
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2023-2025, NVIDIA CORPORATION.  All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-FileCopyrightText: Copyright (c) 2023-2025, NVIDIA CORPORATION.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 #version 460
 
 #extension GL_GOOGLE_include_directive : enable
@@ -56,7 +75,7 @@ void main()
     return;
 #endif
 
-  // increments the visible splat counter in the indirect buffer (second entry of the array)
+  // increments the visible splat counter in the indirect buffer 
   const uint instance_index = atomicAdd(indirect.instanceCount, 1);
   // stores the distance
   distances[instance_index] = encodeMinMaxFp32(-depth);
